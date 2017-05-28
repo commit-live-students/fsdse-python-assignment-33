dc7f0871c4ccdf33c10e8588aac4dd5018370dee
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-05-28 23:16:27 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,14 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    count = 0
+    for listValue in dic:
+        print listValue
+        for charCatch in range(0,len(listValue)):
+            if (listValue[0] == listValue[len(listValue)-1]):
+                count = count + 1
+                break
+
     return count
+
+solution(['abc', 'xyz', 'aba', '1221'])
