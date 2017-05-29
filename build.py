7208e28a7e2ac7e4e6ac8214db4e4bb683c1ab28
rishabh-github <rishabh-github@users.noreply.github.com>
2017-05-29 17:40:56 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,11 @@
 def solution(dic):
-    '''
-    Enter your code here
-    '''
+    count = 0
+    for rec in dic:
+
+        if(len(rec) >= 2):
+            if(rec[:1] == rec[-1:]):
+                count = count + 1
+
     return count
+
+print solution(['abc', 'xyz', 'aba', '1221'])
