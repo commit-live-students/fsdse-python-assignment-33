3db154b405364d2a23d7e80569c8de5bfa8ff795
nikhilakki <nikhilakki@users.noreply.github.com>
2017-06-01 18:20:34 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,11 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    count = 0
+    for i in dic:
+        if i[0] == i[-1]:
+            count+=1
     return count
+
+dic =  ['abc', 'xyz', 'aba', '1221']
+print(solution(dic))
