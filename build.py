90af6fc008169ef12df638e9331c2d29ec524abf
sharduln <sharduln@gmail.com>
2017-06-03 11:26:36 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
-def solution(dic):
+def solution(_list):
     '''
     Enter your code here
     '''
+    count = len([string for string in _list if len(string) >=2 and string[0]==string[-1]])
     return count
