ba20107638dc129fee95e99cc3aad66c07771ada
coded9 <coded9@users.noreply.github.com>
2017-06-03 09:46:41 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,10 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    count = 0
+    for i in dic:
+        if(len(i)>2 and i[0] == i[len(i)-1]):
+            count = count+1;
     return count
+
+#print(solution(['abc', 'xyz', 'aba', '1221']))
