cfd0005a930cf82dff515f54039a8b656376d6fc
sankbhatt <sankbhatt@users.noreply.github.com>
2017-06-03 08:14:03 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
 def solution(dic):
-    '''
-    Enter your code here
-    '''
+    count = 0
+    for  str in dic:
+        if (str[0] == str[len(str)-1]):
+            count += 1
     return count
+
+str = ['abc', 'xyz', 'aba', '1221','abced','123121211']
+print solution(str)
