55990cc562bc5c0bd78b630e3c06b5b8bbd6af78
bhattbhavesh91 <bhattbhavesh91@gmail.com>
2017-06-16 11:23:53 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,32 @@
+'''
+# Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
+
+
+**Notes:**
+
+* Define a function to accept a list as parameter.
+* Iterate over each element and verify if first char and last char are same.
+* If same increment count by one.
+* Return count.
+
+
+**Instructions:**
+* Program should be written in file build.py
+* Function name should be solution.
+* Input
+
+       Type:  List
+       Value: ['abc', 'xyz', 'aba', '1221']
+
+* Expected Output
+
+        Type:  Integer
+        Value: 2
+
+'''
 def solution(dic):
-    '''
-    Enter your code here
-    '''
+    count = 0
+    for val in dic:
+        if val[0] == val[-1]:
+            count +=1
     return count
