81222bbbdc6ccc712a1b9576f743306e4cfaef6c
varunpanicker <varunpanicker@users.noreply.github.com>
2017-06-17 01:13:22 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
 def solution(dic):
-    '''
-    Enter your code here
-    '''
+    count = 0
+
+    for word in dic:
+      if len(word) > 1 and word[0] == word[-1]:
+          count += 1
     return count
+
+print(solution(['abc', 'xyz', 'aba', '1221']))
