b1c5b9a82929231682650ed14dc765b870863de3
adinathauti <adinathauti@gmail.com>
2017-06-19 17:23:06 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
-def solution(dic):
-    '''
-    Enter your code here
-    '''
+def solution(lst):
+    count = 0
+    for e in lst:
+        if e[0] == e[len(e)-1]:
+            count+=1
     return count
+
+t = solution(['abc','xyz','aba','1221'])
+print t
