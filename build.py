eccb9a9835af4ab1cbc2f3e9d314c19d42ae4c6b
arunabh15091989 <Arunabh.social@gmail.com>
2017-06-21 04:05:25 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,8 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    count = 0
+    for i in dic:
+        if isinstance(i, str) and i[0]==i[len(i)-1]:
+            count += 1
     return count
