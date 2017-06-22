8a385043c36e83cf4575eb4afd8fb2311a8e1d57
Bicky23 <bijitdeka23@gmail.com>
2017-06-22 12:14:09 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,10 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    count = 0
+    for element in dic:
+        if element[0] == element[-1]:
+            count += 1
+        else:
+            pass
     return count
