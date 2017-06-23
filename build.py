a67d53ee51ba6dba431e8e563e795b7c9a09ead2
kaushikb11 <kaushikbokka@gmail.com>
2017-06-23 06:56:37 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,10 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    count=0
+    for i in dic:
+        if i[0]==i[-1]:
+            count+=1
+        else:
+            count+=0
     return count
